3964b6d98abb9c71e750fd12544fdd9871945ad0
Shaan VM <shaanmathew18@gmail.com>
2024-03-10 14:50:21 +0530

    add flask code

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+###################################################
+@app.route('/')
+def home():
+    return 'Welcome to regex app'
+
+
+###################################################
+
+
+if __name__=='__main__':
+    app.run(debug=True)
